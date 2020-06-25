113bca75aa0c1d832f31a2e66a8046f9e1ca8d5d
Tomas Fernandez <tfernandez@mulesoft.com>
2020-06-25 12:09:51 -0300

    Refactor: extracted shape detection logic from OasTypeParser to OasTypeDetection

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/OasTypeParser.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/OasTypeParser.scala
@@ -146,72 +146,7 @@ case class OasTypeParser(entryOrNode: Either[YMapEntry, YNode],
     map.key("type").isDefined && map.key("type").get.value.asOption[YSequence].isDefined
   }
 
-  private def detect(version: JSONSchemaVersion): TypeDef = {
-    val defaultType = version match {
-      case oasSchema: OASSchemaVersion if oasSchema.position == "parameter" => UndefinedType
-      case _                                                                => AnyType
-    }
-
-    detectDependency()
-      .orElse(detectType())
-      .orElse(detectObjectProperties())
-      .orElse(detectUnion())
-      .orElse(detectItemProperties())
-      .orElse(detectNumberProperties())
-      .orElse(detectStringProperties())
-      .getOrElse(defaultType)
-  }
-
-  private def detectObjectProperties(): Option[TypeDef.ObjectType.type] =
-    map
-      .key("properties")
-      .orElse(map.key("x-amf-merge"))
-      .orElse(map.key("minProperties"))
-      .orElse(map.key("maxProperties"))
-      .orElse(map.key("dependencies"))
-      .orElse(map.key("patternProperties"))
-      .orElse(map.key("additionalProperties"))
-      .orElse(map.key("discriminator"))
-      .map(_ => ObjectType)
-
-  private def detectItemProperties(): Option[TypeDef.ArrayType.type] =
-    map
-      .key("items")
-      .orElse(map.key("minItems"))
-      .orElse(map.key("maxItems"))
-      .orElse(map.key("uniqueItems"))
-      .map(_ => ArrayType)
-
-  private def detectNumberProperties(): Option[TypeDef.NumberType.type] =
-    map
-      .key("multipleOf")
-      .orElse(map.key("minimum"))
-      .orElse(map.key("maximum"))
-      .orElse(map.key("exclusiveMinimum"))
-      .orElse(map.key("exclusiveMaximum"))
-      .map(_ => NumberType)
-
-  private def detectStringProperties(): Option[TypeDef.StrType.type] =
-    map
-      .key("minLength")
-      .orElse(map.key("maxLength"))
-      .orElse(map.key("pattern"))
-      .orElse(map.key("format"))
-      .map(_ => StrType)
-
-  private def detectDependency(): Option[TypeDef] = map.key("$ref").map(_ => LinkType)
-
-  private def detectUnion(): Option[TypeDef.UnionType.type] = map.key("x-amf-union").map(_ => UnionType)
-
-  private def detectType(): Option[TypeDef] = map.key("type").flatMap { e =>
-    val t      = e.value.as[YScalar].text
-    val f      = map.key("format").flatMap(e => e.value.toOption[YScalar].map(_.text)).getOrElse("")
-    val result = matchType(t, f, UndefinedType)
-    if (result == UndefinedType) {
-      ctx.eh.violation(InvalidJsonSchemaType, "", s"Invalid type $t", e.value)
-      None
-    } else Some(result)
-  }
+  private def detect(version: JSONSchemaVersion): TypeDef = new OasTypeDetection(map).detect(version)
 
   private def parseDisjointUnionType(): UnionShape = {
 
